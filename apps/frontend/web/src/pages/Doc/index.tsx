cecb4d950d9928154dd5e2eae44024bcd743b9d9
WeMadeCode <miaomaedu@163.com>
2025-08-20 22:27:31 +0800

    update

--- apps/frontend/web/src/pages/Doc/index.tsx
+++ apps/frontend/web/src/pages/Doc/index.tsx
@@ -35,8 +35,9 @@ import { DocEditor } from './DocEditor'
 
 const doc = new Y.Doc()
 // const provider = new WebsocketProvider('ws://localhost:8082', `doc-yjs`, doc)
-const wsHost = import.meta.env.VITE_WS_HOST ?? '192.168.31.251'
-const provider = new WebsocketProvider(`ws://${wsHost}:8082`, `doc-yjs`, doc, { connect: false })
+const wsHost = import.meta.env.VITE_WS_HOST ?? window.location.hostname
+const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
+const provider = new WebsocketProvider(`${wsProtocol}://${wsHost}:8082`, `doc-yjs`, doc, { connect: false })
 
 export const Doc = () => {
   const params = useParams()
